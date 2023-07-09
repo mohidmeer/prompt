ae1897d4e7fcc76de5927fd23213f3069f76f5a0
Mohid Meer <mohidfauji@gmaiol.com>
2023-07-09 14:06:18 -0700

    Replaced Hereo section

--- pages/index.js
+++ pages/index.js
@@ -29,23 +29,9 @@ export default function Home() {
       <div className='mt-8 border-2 rounded-xl  p-4 lg:pb-8 border-white '> <Prompts title={'Midjourney'} /> </div>
       <div className='mt-8 border-2 rounded-xl  p-4 lg:pb-8 border-white '> <Prompts title={'Stable Diffusion'} /> </div>
       <div className='mt-8 border-2 rounded-xl  p-4 lg:pb-8 border-white '> <Prompts title={'Dalle'} /> </div>
-<<<<<<< HEAD
       {/* <div className='mt-8 border-2 rounded-xl  p-4 lg:pb-8 border-white '> <Enginers/> </div> */}
-=======
       <div className='mt-8 border-2 rounded-xl  p-4 lg:pb-8 border-white '> <Prompts title={'Chat Gpt'} /> </div>
       <div className='mt-8 border-2 rounded-xl  p-4 lg:pb-8 border-white '> <Enginers/> </div>
->>>>>>> 86bc1610000e623cc293694081cb9227392e076e
-
-
-
-
-
-
-
-
-
-
-
 
 
 
