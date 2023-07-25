83581c55bc7f960e03d8afd0d5d5ab263bb05ea0
Mohid Meer <mohidfauji@gmail.com>
2023-07-25 10:05:52 -0700

    Added single prompt display Ui

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,31 @@ import PromptContainer from "@/components/user/PromptContainer";
 import Hero from "@/components/user/hero";
 import AppLayout from "@/layout/AppLayout";
 import { useExplore } from "@/stores/explore";
+import { CldImage } from "next-cloudinary";
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from "react";
+import { AiFillEye, AiFillLike } from "react-icons/ai";
+import { BiHeart } from "react-icons/bi";
+import { BsFillHeartFill } from "react-icons/bs";
 export default function Home() {
 
   const {products,fetchProductData}=useExplore();
-  const [loading,setLoading]=useState();
+  const [loading,setLoading]=useState();  
 
-  const prompts= [
-    { id:1 , name: 'Architecture Design', stars:5  },
-]
+  const Heights=[250,350,200,320,280]
+  function getRandomHeight() {
+    const randomIndex = Math.floor(Math.random() * Heights.length);
+    return Heights[randomIndex];
+  }
 
-  useEffect(()=>{ fetchProductData().then(()=>{setLoading(false);console.log(products)  })},[])
+  useEffect(()=>{ fetchProductData().then(()=>{setLoading(false);})},[])
+
+
+  useEffect(()=>{
+    console.log(products.length)
+  },[loading])
 
   return (
     <AppLayout>
@@ -23,19 +35,67 @@ export default function Home() {
         <title>PromptWorks | Marketpalace</title>
         <meta name="description" content='' />
       </Head>
-      <div className='grid grid-cols-4 gap-4'>
-          {Array.from({ length: Math.ceil(prompts.length / 4) }).map((_, rowIndex) => (
+      { loading ?  '' :
+      
+      <div className='grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-col-1 gap-4 bg-black p-2 w-1/2   sm:w-full mx-auto  '  >
+          {Array.from({ length: Math.ceil(products.length / 5) }).map((_, rowIndex) => (
             <div key={rowIndex} className='flex flex-col gap-4'>
-              {prompts.slice(rowIndex * 4, rowIndex * 4 + 4).map((p, i) => (
-                <div key={i} className='rounded bg-blue-600 h-32'>
-                  dark {rowIndex * 4 + i}{'First loop'+ rowIndex}
-                </div> 
+              {products.slice(rowIndex * 5, rowIndex * 5 + 5).map((p, i) => (
+                <Link href={'/prompt/'+p.slug} key={i} className='rounded border border-dark-border relative '>
+                  <Details p={p} i={i} row={rowIndex}/>
+                  <CldImage src={p.images[0]}
+                      width={300}
+                      height={getRandomHeight()}
+                      crop="fill"
+                      alt={p.name}
+                      draggable='false'
+                      unselectable='off' 
+                      />
+                </Link> 
               ))}
             </div>
           ))}
          </div>
+      }
 
       
     </AppLayout>
+  )
+}
+
+
+const Details = ({p,i,row}) => {
+  return (
+    <>
+      <div className="absolute top-2 left-2 text-xs backdrop-blur-md ">
+        <p className="p-1 bg-dark-info font-bold rounded ">{p.model.replace(/-/g, ' ')}</p>
+      </div>
+      <div className=" absolute bottom-0 w-full  ">
+         <div class="absolute inset-0 bg-opacity-50 backdrop-blur-sm bg-black "></div>  
+        <p className="relative z-10 text-white px-1  ">{p.name}</p>
+
+        <div className="relative" >
+      <div class="absolute inset-0 bg-opacity-50 backdrop-blur-sm bg-black "></div>  
+      <div class="absolute inset-0 bg-opacity-50 backdrop-blur-sm bg-black "></div>  
+      <div className=" relative flex gap-2  text-sm p-2">
+          <span className="flex items-center gap-1 hover:bg-dark-muted px-2 rounded-md cursor-pointer ">
+            <BsFillHeartFill className="text-red-500" />
+            <p >{p.favourites}</p>
+          </span>
+          <span className="flex items-center gap-1   hover:bg-dark-muted px-2 rounded-md cursor-pointer ">
+            <AiFillLike   className="text-yellow-500 " />
+            <p >{p.favourites}</p>
+          </span>
+          <span className="flex items-center gap-1   hover:bg-dark-muted px-2 rounded-md cursor-pointer ">
+            <AiFillEye className='text-white'/>
+            <p>0</p>
+          </span>
+        </div>
+
+      </div>
+      </div>
+      
+    
+    </>
   )
 }
